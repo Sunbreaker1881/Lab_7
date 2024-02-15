f63da14f2eac53e28d8a7d474c5e8a2b6d93bf96
Sunbreaker1881 <e1p8s@aol.com>
2024-02-15 15:09:27 -0500

    initial commit

---
+++ lab_assignment_5.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	char letter;
+	struct node* next;
+} node;
+
+// Returns number of nodes in the linkedList.
+int length(node* head)
+{
+	int counter = 0;
+	node * tptr = head;
+	while(tptr != NULL)
+	{
+		counter++;
+		tptr = tptr->next;
+	}
+	return counter;
+}
+
+// parses the string in the linkedList
+//  if the linked list is head -> |a|->|b|->|c|
+//  then toCString function wil return "abc"
+char* toCString(node* head)
+{
+	int slength = length(head);
+	char *totalstring = (char*)malloc((slength + 1) * sizeof(char));
+	node * newnode = head;
+	int i = 0;
+	while (newnode != NULL)
+	{
+		totalstring[i] = newnode->letter;
+		newnode = newnode->next;
+		i++;
+	}
+	totalstring[i] = '\0';
+	return totalstring;
+}
+
+// inserts character to the linkedlist
+// f the linked list is head -> |a|->|b|->|c|
+// then insertChar(&head, 'x') will update the linked list as foolows:
+// head -> |a|->|b|->|c|->|x|
+void insertChar(node** pHead, char c)
+{
+	node *temp = (node*)malloc(sizeof(node));
+	temp->letter = c;
+	temp->next = NULL;
+	if (*pHead == NULL)
+	{
+		*pHead = temp;
+	}
+	else
+	{
+		node * current = *pHead;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = temp;
+	}
+}
+
+// deletes all nodes in the linkedList.
+void deleteList(node** pHead)
+{
+	while (*pHead != NULL)
+	{
+		node* temp = *pHead;
+		*pHead = (*pHead)->next;
+		free(temp);
+	}
+}
+
+int main(void)
+{
+	int i, strLen, numInputs;
+	node* head = NULL;
+	char* str;
+	char c;
+	FILE* inFile = fopen("input.txt","r");
+	if (inFile != NULL)
+	{
+		printf("this worked");
+	}
+
+	fscanf(inFile, " %d\n", &numInputs);
+	
+	while (numInputs-- > 0)
+	{
+		fscanf(inFile, " %d\n", &strLen);
+		for (i = 0; i < strLen; i++)
+		{
+			fscanf(inFile," %c", &c);
+			insertChar(&head, c);
+		}
+
+		str = toCString(head);
+		printf("String is : %s\n", str);
+
+		free(str);
+		deleteList(&head);
+
+		if (head != NULL)
+		{
+			printf("deleteList is not correct!");
+			break;
+		}
+	}
+
+	fclose(inFile);
+}
